51766107092ffb29d2cd2c9cecfd877dda55ef9a
Emanuel Kywal <emanuelkywal@outlook.com>
2023-11-23 22:01:20 -0300

    feat: add hover effect on generateMatrix box's border

--- scripts/updateIcon.js
+++ scripts/updateIcon.js
@@ -1,14 +1,16 @@
 
 function changeIcon(buttonHTML) {
     let img = buttonHTML.getElementsByTagName("img");
-    // console.log(img);
+    let form_box = document.getElementsByClassName("matrix-dimension");
 
+    form_box[0].style.borderColor = "black";
     img[0].src = "../assets/imgs/matrix-white-icon.svg";
 }
 
 function unchangeIcon(buttonHTML) {
     let img = buttonHTML.getElementsByTagName("img");
-    // console.log(img);
+    let form_box = document.getElementsByClassName("matrix-dimension");
 
+    form_box[0].style.borderColor = "white";
     img[0].src = "../assets/imgs/matrix-icon.svg";
 }
